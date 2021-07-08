ebff2f606a6f009b2803b2bad22cdeb6928daf8a
mcp48 <michael.c.payne83@gmail.com>
2021-07-07 22:48:56 -0400

    Feature: Created CSVReader class

---
+++ CSVReader/csv_reader.py
@@ -0,0 +1,19 @@
+import csv
+from AdditionalModules.absolute_path import absolute_path
+from AdditionalModules.type_checker import is_valid_number
+
+
+class CSVReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+
+        with open(absolute_path(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                for k, v in row.items():
+                    num = v
+                    is_valid_number(num)
+                self.data.append(row)
+        pass
