99c6d7882169e3990c7966e06254420734ae38f6
mcp48 <michael.c.payne83@gmail.com>
2021-07-08 15:15:05 -0400

    Feature: Added subtraction method to unit test file

--- UnitTests/test_calculator.py
+++ UnitTests/test_calculator.py
@@ -14,5 +14,11 @@ class MyTestCase(unittest.TestCase):
     def test_add_method_calculator(self):
         test_data = CSVReader('./UnitTests/TestData/Unit_Tests_Addition.csv').data
         for row in test_data:
-            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), float(row['Result']))
-            self.assertEqual(self.calculator.result, float(row['Result']))
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_subtract_method_calculator(self):
+        test_data = CSVReader('./UnitTests/TestData/Unit_Tests_Subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
