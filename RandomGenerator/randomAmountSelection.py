98bdb07c3814776b5abfe24f68b2a1b1bfd0701c
mcp48 <michael.c.payne83@gmail.com>
2021-07-08 16:23:42 -0400

    Feature: Created random package and randomAmountSelection.py file

---
+++ RandomGenerator/randomAmountSelection.py
@@ -0,0 +1,14 @@
+from numpy import random
+
+
+def random_amount_selection(list, num_values):
+    try:
+        return_values = []
+        for num in range(num_values):
+            rand_selection = random.choice(list)
+            return_values.append(rand_selection)
+        return return_values
+
+    except ValueError as error:
+        error = "ERROR!  That is an empty array.  Try again."
+        return error
