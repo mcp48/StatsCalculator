140854c524adb4f814416075eaf8b84d92239736
mcp48 <michael.c.payne83@gmail.com>
2021-07-08 15:21:11 -0400

    Feature: Added multiplication method to test_calculator.py file

--- UnitTests/test_calculator.py
+++ UnitTests/test_calculator.py
@@ -22,3 +22,9 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
+
+    def test_multiply_method_calculator(self):
+        test_data = CSVReader('./UnitTests/TestData/Unit_Tests_Multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
