59bc05c45549257b8ab8d5a92a2d11c1e60b60b6
mcp48 <michael.c.payne83@gmail.com>
2021-07-10 15:03:35 -0400

    Task: Created mode calculation file

---
+++ StatsCalculations/mode.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+def mode(data):
+    try:
+        total_values = len(data)
+        count = Counter(data)
+        get_mode = dict(count)
+        mode = [a for a, v in get_mode.items() if v == max(list(count.values()))]
+
+        if len(mode) == total_values:
+            get_mode = "Mode not found"
+
+        else:
+            get_mode = mode[0]
+
+        return get_mode
+
+    except ValueError:
+        print("ERROR! That is an empty array. Try again.")
