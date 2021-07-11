80adc6f684ba656a54ddc5718dac33ce829aed58
mcp48 <michael.c.payne83@gmail.com>
2021-07-11 17:34:49 -0400

    Fix: Changed assert AlmostEqual to AssertEqual in test variance method

--- UnitTests/test_statistics.py
+++ UnitTests/test_statistics.py
@@ -12,7 +12,7 @@ class MyTestCase(unittest.TestCase):
         self.testData = [int(row['Value']) for row in self.allData]
         self.testAnswers = CSVReader('./UnitTests/TestData/Stats_Answers.csv').data
         self.list = random_integer_list(1, 100, 20, 10)
-        self.num_val = 4
+        # self.num_val = 4
 
     def test_instantiate_stats_calculator(self):
         self.assertIsInstance(self.statsCalc, StatisticsCalculator)
@@ -34,8 +34,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_variance_method(self):
         var_test_val = (var(self.testData))
-        self.assertAlmostEqual(self.statsCalc.variance(self.testData), var_test_val)
-        self.assertAlmostEqual(self.statsCalc.result, var_test_val)
+        self.assertEqual(self.statsCalc.variance(self.testData), var_test_val)
+        self.assertEqual(self.statsCalc.result, var_test_val)
 
     def test_standard_deviation(self):
         std_test_val = (std(self.testData))
