28437f11fb3fd3cabbdaef3a382a7c68f4ddae0c
mcp48 <michael.c.payne83@gmail.com>
2021-07-11 17:41:21 -0400

    Feature: Added sources to statistics calculator file

--- StatsCalculations/statisticsCalculator.py
+++ StatsCalculations/statisticsCalculator.py
@@ -29,3 +29,9 @@ class StatisticsCalculator(Calculator):
     def standard_deviation(self, data):
         self.result = standard_deviation(data)
         return self.result
+
+
+# Source for mean, median, and mode:
+#   https://www.geeksforgeeks.org/finding-mean-median-mode-in-python-without-libraries/
+# Source for variance: https://www.geeksforgeeks.org/python-variance-of-list/
+# Source for standard deviation: https://www.geeksforgeeks.org/python-standard-deviation-of-list/
